e484f0e236e0d0a2adc41ecc3437848a932ad515
Divyansh Anand <57003965+divyansh0908@users.noreply.github.com>
2024-03-23 23:10:10 +0530

    feat: Add scrapper for applied jobs on linkedin

--- app.py
+++ app.py
@@ -8,73 +8,97 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import time
-import os
-from dotenv import load_dotenv
+import csv
 
-load_dotenv()
+def create_csv(filename, headers):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(headers)
 
-myId=os.getenv("EMAIL")
-myPassword=os.getenv("PASSWORD")
+def add_row(filename, data):
+    with open(filename, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(data)
+
+# Define the filename for the CSV file
+filename = 'data.csv'
+
+# Define headers for the CSV file
+headers = ['Company Name', 'Position', 'Location', 'Link']
+# Create the CSV file with headers
+create_csv(filename, headers)
+
+myId = "email id"
+myPassword = "pass"
 opt = Options()
 opt.add_argument('--disable-blink-features=AutomationControlled')
 opt.add_argument('--start-maximized')
-opt.add_extension('IDM-Integration-Module.crx')
-driver = webdriver.Chrome(executable_path='chromedriver.exe', options=opt)
-driver.get('https://sleepdata.org/datasets/mros/files/polysomnography/edfs/visit1?page=8')
+driver = webdriver.Chrome()
+driver.get('https://www.linkedin.com/my-items/saved-jobs/?cardType=APPLIED')
+
 
 def checkLoginState():   # Check if user is logged in
     try:
-        driver.find_element(By.XPATH,"//a[@href='/login']")
-        
+        isPresentt = driver.find_element(By.XPATH,"//button[@aria-label='Sign in']")
+        print("isPresentt", isPresentt)
         isPresent = True;
     except:
         isPresent = False;
     return isPresent;
-
+checkLoginState()
+# time.sleep(3)
 def Login():                # Login to sleepdata.org
-    driver.find_element(By.XPATH,"//input[@id='user_email']").send_keys(myId)
-    driver.find_element(By.XPATH,"//input[@id='user_password']").send_keys(myPassword)
-    driver.find_element(By.XPATH,"//input[@type='submit']").click()
+    driver.find_element(By.XPATH,"//input[@id='username']").send_keys(myId)
+    driver.find_element(By.XPATH,"//input[@id='password']").send_keys(myPassword)
+    driver.find_element(By.XPATH,"//button[@aria-label='Sign in']").click()
+# Login()
+# time.sleep(1000)
 print(checkLoginState())
+def goToNextPage():
+    driver.find_element(By.XPATH,"//button[@aria-label='Next']").click()
+
+def getData():
+    print('📊📊📊📊📊')
+    allPositions = driver.find_elements(By.XPATH, "//div[@class='mb1']/div[1]/div/span/span/a")
+    allCompany = driver.find_elements(By.XPATH, "//div[@class='mb1']/div[2]")
+    allLocations = driver.find_elements(By.XPATH, "//div[@class='mb1']/div[3]")
+    # /div/div/span/span/a
+    print('length', len(allPositions))
+    data = []
+    for i in range(10):
+        # position = i.find_element(By.XPATH, "//div[1]")
+        company = allCompany[i].text
+        location = allLocations[i].text
+        position = allPositions[i].text
+        link = allPositions[i].get_attribute('href')
+        add_row(filename, [company, position, location, link])
+        print('📊📊📊📊📊', company, location, position, link)
 #calling Login function if user is not logged in to sleepdata.org
 if(checkLoginState()==False):
     print("Already logged in")
 else:
-    driver.find_element(By.XPATH,"//a[@href='/login']").click()
     try:
-        myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="sign-up-form-title"]')))
         Login()
         print("Page is ready!")
+        time.sleep(2)
+        getData()
+        for j in range(21):
+            try: 
+                goToNextPage()
+                time.sleep(5)
+                getData()
+            except: 
+                print("Error in going to next page")
+
     except TimeoutException:
         print ("Loading took too much time!")
     print("Logged in")
-#function to check if file is already downloaded
-def checkFile():
-    pathOfVisit1="D:\\DSP\\mros\\polysomnography\\annotations-events-profusion\\visit1"
-    fileList=os.listdir(pathOfVisit1)
-    return fileList
-#function to download file
-def logLinks():
-    for i in range(1,31):
-        driver.get('https://sleepdata.org/datasets/mros/files/polysomnography/annotations-events-profusion/visit1?page='+str(i))
-        time.sleep(1)
-        links = driver.find_elements(By.XPATH,"//a[@data-turbolinks='false']")
-        listFile=checkFile()
-        for link in links:
-            fileName=link.get_attribute('innerHTML')
-            if(fileName in listFile):
-                print("File already downloaded")
-                pass
-            else:
-                print("Downloading file: "+fileName)
-                try:
-                    link.click()   
-                except:
-                    try:
-                        link.click()
-                        time.sleep(1)
-                    except:
-                        pass
 
-logLinks()
+
+
+
+
+
+
+
+time.sleep(1000)
